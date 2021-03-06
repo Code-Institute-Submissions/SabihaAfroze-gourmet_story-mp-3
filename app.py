ddf01e18f2c158519e13a80e82375eafaa91c502
Sabiha <fosabiha@gmail.com>
2021-03-06 02:50:00 +0000

    Add Register functionality

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
+    Flask, Blueprint, render_template, redirect,
+    request, url_for, flash, session, Markup)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash,check_password_hash
@@ -24,8 +24,25 @@ def get_recipes():
     return render_template("recipes.html",recipes=recipes)
 
 
-@app.route("/register",methods=["GET","POST"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
 
 

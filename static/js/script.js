a78c85f4c720cea34bf33613cf4846d76d629c3d
Sabiha <fosabiha@gmail.com>
2021-03-07 22:34:13 +0000

    recipes.html has been developed

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
  $(document).ready(function () {
-     
+     /*---mobile navbar---*/
+
+    $('.sidenav').sidenav();
+
     /*---id year for 'Copyright' in <footer>---*/
+    
     $("#year").html(new Date().getFullYear());
 
  });
